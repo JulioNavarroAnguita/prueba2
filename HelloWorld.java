5d2785fd79fa747bbe41e707f11191414bed18c7
Julio Navarro <julio.navarro.anguita@gmail.com>
2017-10-26 14:16:33 +0200

    New commit, HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.printf("%s%n", "Hola Mundo");
+        System.out.printf("Hola Mundo");
     }
 }
